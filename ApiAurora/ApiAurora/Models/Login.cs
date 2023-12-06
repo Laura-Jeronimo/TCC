55dc5983e938b5ed681e250c1062b07995dc412b
laura-jeronimo <laurajeroes@gmail.com>
2023-12-06 17:48:41 -0300

    .

--- ApiAurora/ApiAurora/Models/Login.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiAurora.Models
-{
-    public class Login
-    {
-        public int Id { get; set; } 
-        public string Email { get; set; }
-        public string Senha {  get; set; }
-    }
-}
